0f270921fb60c52b80c99c145255fea16434e1ec
jmneko <new-neko@hotmail.com>
2023-11-02 00:08:42 +0100

    Creacion de rutas

--- src/app.js
+++ src/app.js
@@ -7,4 +7,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use("/api", require("./routes/api"));
+
 module.exports = app;
---
+++ src/controllers/inmuebles.controller.js
@@ -0,0 +1,14 @@
+const getInmuebles = (req, res) => {};
+
+const createInmueble = (req, res) => {};
+
+const updateInmueble = (req, res) => {};
+
+const deleteInmueble = (req, res) => {};
+
+module.exports = {
+  getInmuebles,
+  createInmueble,
+  updateInmueble,
+  deleteInmueble,
+};
