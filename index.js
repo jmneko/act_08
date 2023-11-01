cb9569d03a85660d28617f228790eb156142e771
jmneko <new-neko@hotmail.com>
2023-11-01 23:35:39 +0100

    Configuracion de API

---
+++ index.js
@@ -0,0 +1,14 @@
+const http = require("node:http");
+const app = require("./src/app");
+
+require("dotenv").config();
+
+const server = http.createServer(app);
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT);
+
+server.on("listening", () =>
+  console.log(`Servidor escuchado en puerto ${PORT}`)
+);
+server.on("error", () => console.log(error));
