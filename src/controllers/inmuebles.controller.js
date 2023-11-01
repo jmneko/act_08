191bde6e46e4aa2a664a608b2b42baf85a917a98
jmneko <new-neko@hotmail.com>
2023-11-02 00:24:26 +0100

    creando rutas de las funciones

--- src/controllers/inmuebles.controller.js
+++ src/controllers/inmuebles.controller.js
@@ -1,10 +1,18 @@
-const getInmuebles = (req, res) => {};
+const getInmuebles = (req, res) => {
+  res.json("GET");
+};
 
-const createInmueble = (req, res) => {};
+const createInmueble = (req, res) => {
+  res.json("POST");
+};
 
-const updateInmueble = (req, res) => {};
+const updateInmueble = (req, res) => {
+  res.json("PUT");
+};
 
-const deleteInmueble = (req, res) => {};
+const deleteInmueble = (req, res) => {
+  res.json("DELETE");
+};
 
 module.exports = {
   getInmuebles,
--- src/routes/api/inmuebles.js
+++ src/routes/api/inmuebles.js
@@ -1,3 +1,10 @@
 const router = require("express").Router();
 
+const InmueblesController = require("../../controllers/inmuebles.controller");
+
+router.get("/", InmueblesController.getInmuebles);
+router.post("/", InmueblesController.createInmueble);
+router.put("/:inmuebleId", InmueblesController.updateInmueble);
+router.delete("/:inmuebleId", InmueblesController.deleteInmueble);
+
 module.exports = router;
